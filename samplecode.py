fc67b74ba4ccc569ddfd3fe43f47e4e431151cf2
Devakrishna C Nair <devakrishna10@gmail.com>
2022-04-21 14:19:00 +0530

    Add files via upload
    added sample code with exposed AWS api keys

---
+++ samplecode.py
@@ -0,0 +1,13 @@
+print("hello world")
+
+aws_key = "AKIAYVP4CIPPJMUFBWVP"
+aws_secret = "dgVTlMUi9c2gBDOjsr3M3LM/eON6jHFJoNqSf6Zi"
+
+print("sdklfj")
+
+aws_key = "AKIAYVP4CIPPJMUFBWVP"
+aws_secret = "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"
+
+
+aws_key = "AKIAYVP4CIPPJMUFBXVP"
+aws_secret = "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"
